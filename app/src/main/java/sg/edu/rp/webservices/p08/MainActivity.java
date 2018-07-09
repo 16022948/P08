4bb9394bff1ed8d9fb4c2e6dafb74f7d7a1e0ef9
LIM YONG <16022948@rp.edu.sg>
2018-07-09 16:20:30 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/webservices/p08/MainActivity.java
@@ -0,0 +1,117 @@
+package sg.edu.rp.webservices.p08;
+
+import android.Manifest;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1, btn2, btn3;
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        SupportMapFragment mapFragment = (SupportMapFragment) fm.findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+
+                LatLng poi_north = new LatLng(1.429808, 103.779949);
+                Marker north = map.addMarker(new
+                        MarkerOptions()
+                        .position(poi_north)
+                        .title("HQ - North")
+                        .snippet("Block 333, Admiralty Ave 3, 765654\n" +
+                                "Operating hours: 10am-5pm\n" +
+                                "Tel:65433456\n")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW))
+                );
+
+                LatLng poi_center = new LatLng(1.297662, 103.847486);
+                Marker center = map.addMarker(new
+                        MarkerOptions()
+                        .position(poi_center)
+                        .title("HQ - Center")
+                        .snippet("Block 3A, Orchard Ave 3, 134542 \n" +
+                                "Operating hours: 11am-8pm\n" +
+                                "Tel:67788652\n")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+                );
+
+                LatLng poi_east = new LatLng(1.350000, 103.93383);
+                Marker east = map.addMarker(new
+                        MarkerOptions()
+                        .position(poi_east)
+                        .title("HQ - East")
+                        .snippet("Block 555, Tampines Ave 3, 287788 \n" +
+                                "Operating hours: 9am-5pm\n" +
+                                "Tel:66776677\n")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                );
+
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(poi_north,15));
+                UiSettings ui = map.getUiSettings();
+                ui.setCompassEnabled(true);
+
+                int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+                if (permissionCheck == PermissionChecker.PERMISSION_GRANTED) {
+                    map.setMyLocationEnabled(true);
+                }else{
+                    Log.e("GMap - Permission","GPS access has not been granted");
+                }
+            }
+        });
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
+        btn3 = (Button)findViewById(R.id.btn3);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (map != null) {
+                    map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                }
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (map != null) {
+                    map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                }
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (map != null) {
+                    map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                }
+            }
+        });
+
+    }
+}
